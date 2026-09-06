dbab03c42ac4308e6b4739cf44d6e848098ef064
BondSensei <phuphamwork89@gmail.com>
2026-09-06 16:52:55 +0700

    Finish Project

---
+++ To_Do_List/Web/Controllers/TodoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using To_Do_List.Core.Interfaces;
+using To_Do_List.Core.Models;
+using To_Do_List.Web.ViewModels;
+
+namespace To_Do_List.Web.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var todos = await _todoService.GetAllTodoAsync();
+            return View(todos);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ToDoCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var todo = new ToDoItem()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                DueAt = (DateTime)model.DueDate,
+                CreatedAt = DateTime.Now
+            };
+
+            await _todoService.CreateTodoAsync(todo);
+            TempData["Success"] = "Thêm công việc thành công!";
+            return RedirectToAction(nameof(Index));
+
+        }
+
+    }
+}
---
+++ To_Do_List/Web/ViewModels/ToDoCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_Do_List.Web.ViewModels
+{
+    public class ToDoCreateViewModel
+    {
+        [Required(ErrorMessage = "Title cannot be empty!")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
